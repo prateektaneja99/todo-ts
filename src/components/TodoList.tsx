60559d9a854d4282505353daf446eb0672dbbc5d
Prateek Taneja <75838387+prateektaneja99@users.noreply.github.com>
2023-07-20 17:58:50 -0700

    first commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
+
+interface props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<props> = ({ todos, setTodos }) => {
+  return (
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">Active Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+      <div className="todos remove">
+      <span className="todos__heading">Completed Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
